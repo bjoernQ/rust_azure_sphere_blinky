66539fe9ee2b6e92a5d15e881e20ef35abc3c6d3
bjoern <bjoern.quentin@mobile-j.de>
2018-12-20 12:06:43 +0100

    add package batch script

---
+++ sphere_blinky/package.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+set AZSPHERETOOLS=c:\Program Files (x86)\Microsoft Azure Sphere SDK\Tools\
+set PATH=%PATH%;%AZSPHERETOOLS%
+
+mkdir target\approot\bin
+copy target\arm-v7-none-eabi\debug\BlinkySphere target\approot\bin\app
+copy app_manifest.json target\approot
+
+azsphere image package-application --input target\approot --output target\manual.imagepackage --sysroot 1 -v
+
+echo "Now do this:"
+echo "azsphere device sideload delete"
+echo "azsphere device sideload deploy -p manual.imagepackage"
+echo
+
