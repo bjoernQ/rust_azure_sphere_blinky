60290383425742a1e8a83578b32661ef99c91159
bjoern <bjoern.quentin@mobile-j.de>
2018-12-23 20:36:27 +0100

    improvements

--- gcc_wrapper/src/main.rs
+++ gcc_wrapper/src/main.rs
@@ -2,13 +2,26 @@ use std::env;
 use std::process::Command;
 use std::process;
 use std::str;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut args: Vec<String> = env::args().collect();
 
     let mut args_to_gcc: Vec<String> = Vec::new();
     let mut idx = 0;
     let mut take_next = false;
+
+    if args[1].starts_with("@") {
+        let filename = &args[1][1..];
+        let file = File::open(filename).expect("unable to open linker args file");
+        let buf = BufReader::new(file);
+        let mut lines: Vec<String> = buf.lines()
+            .map(|l| l.expect("Could not parse line"))
+            .collect();
+        args.append(&mut lines);
+    }
 
     for arg in args {
         if idx == 0 {
@@ -39,11 +52,15 @@ fn main() {
             args_to_gcc.push(arg.clone());
         }
 
+        if arg.ends_with(".rlib") {
+            args_to_gcc.push(arg.clone());
+        }
+
         idx = idx +1;
     };
 
     args_to_gcc.push("--sysroot=C:\\Program Files (x86)\\Microsoft Azure Sphere SDK\\Sysroots\\1".to_string());
-    args_to_gcc.push("-Wl,--no-undefined".to_string());
+    args_to_gcc.push("-Wl,--no-undefined,--gc-sections".to_string());
     args_to_gcc.push("-nodefaultlibs".to_string());
     args_to_gcc.push("-B".to_string());
     args_to_gcc.push("C:\\Program Files (x86)\\Microsoft Azure Sphere SDK\\Sysroots\\1\\tools\\gcc".to_string());
@@ -56,6 +73,7 @@ fn main() {
     args_to_gcc.push("-lpthread".to_string());
     args_to_gcc.push("-lgcc_s".to_string());
     args_to_gcc.push("-lc".to_string());
+    args_to_gcc.push("-Os".to_string());
 
     eprintln!("args to gcc {:?}", args_to_gcc);
 
@@ -64,150 +82,14 @@ fn main() {
             .output()
             .expect("failed to execute process");
 
-    let out = output.stdout;
-    //println!("{}", out);
+    let out = str::from_utf8(&(output.stdout)).expect("unable to get stdout");
+    eprintln!("{}", out);
 
     let err_out = str::from_utf8(&(output.stderr)).expect("unable to get stderr");
     eprintln!("{}", err_out);
 
-    process::exit(0);
+    match output.status.code() {
+        Some(code) => process::exit(code),
+        None       => process::exit(42)
+    }
 }
-
-/*
-setlocal
-set AZSPHERETOOLS=c:\Program Files (x86)\Microsoft Azure Sphere SDK\Tools\
-set SYSROOT=C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1
-set CCPATH=%SYSROOT%\tools\gcc
-set CLANGPATH=D:\.konan\dependencies\msys2-mingw-w64-x86_64-gcc-7.3.0-clang-llvm-lld-6.0.1\bin
-set PATH=%PATH%;%CCPATH%;%CLANGPATH%;%AZSPHERETOOLS%
-
-gcc -o "Blink.out" --sysroot="%SYSROOT%" -Wl,--no-undefined -nodefaultlibs -B "%SYSROOT%\tools\gcc" -march=armv7ve -mcpu=cortex-a7 -mthumb -mfpu=neon -mfloat-abi=hard hello.o -lapplibs -lpthread -lgcc_s -lc
-gcc -o "Blink.out" hello.o --sysroot="%SYSROOT%" -Wl,--no-undefined -nodefaultlibs -B "%SYSROOT%\tools\gcc" -march=armv7ve -mcpu=cortex-a7 -mthumb -mfpu=neon -mfloat-abi=hard -lapplibs -lpthread -lgcc_s -lc
-
-
--o "Blink.out"
-hello.o
-
---sysroot="%SYSROOT%"
--Wl,--no-undefined
--nodefaultlibs
--B "%SYSROOT%\tools\gcc"
--march=armv7ve
--mcpu=cortex-a7
--mthumb
--mfpu=neon
--mfloat-abi=hard
--lapplibs
--lpthread
--lgcc_s
--lc
-
-
-*/
-
-
-/*
- = note: "cmd" "/c" "link.bat" "-L" "C:\\Users\\bjoern\\.xargo\\lib\\rustlib\\arm-v7-none-eabi\\lib" "D:\\workspaces\\test\\target\\arm-v7-none-eabi\\debug\\deps\\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o" "-o" "D:\\workspaces\\test\\target\\arm-v7-none-eabi\\debug\\deps\\BlinkySphere-e957dd655fdee2c4" "-Wl,--gc-sections" "-nodefaultlibs" "-L" "D:\\workspaces\\test\\target\\arm-v7-none-eabi\\debug\\deps" "-L" "D:\\workspaces\\test\\target\\debug\\deps" "-L" "C:\\Users\\bjoern\\.xargo\\lib\\rustlib\\arm-v7-none-eabi\\lib" "-Wl,-Bstatic" "C:\\Users\\bjoern\\.xargo\\lib\\rustlib\\arm-v7-none-eabi\\lib\\libcore-79b7ca805d23dd23.rlib" "C:\\Users\\bjoern\\.xargo\\lib\\rustlib\\arm-v7-none-eabi\\lib\\libcompiler_builtins-5974d7451bf4ffc6.rlib" "-Wl,-Bdynamic"
-  = note: lala -L C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o -o D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4 -Wl,--gc-sections -nodefaultlibs -L D:\workspaces\test\target\arm-v7-none-eabi\debug\deps -L D:\workspaces\test\target\debug\deps -L C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib -Wl,-Bstatic C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib\libcore-79b7ca805d23dd23.rlib C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib\libcompiler_builtins-5974d7451bf4ffc6.rlib -Wl,-Bdynamic
-          args -L
-          args C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib
-          args D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o
-          args -o
-          args D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4
-          args -Wl
-          args --gc-sections
-          args -nodefaultlibs
-          args -L
-          args D:\workspaces\test\target\arm-v7-none-eabi\debug\deps
-          args -L
-          args D:\workspaces\test\target\debug\deps
-          args -L
-          args C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib
-          args -Wl
-          args -Bstatic
-          args C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib\libcore-79b7ca805d23dd23.rlib
-          args C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib\libcompiler_builtins-5974d7451bf4ffc6.rlib
-          args -Wl
-          args -Bdynamic
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-          Datei nicht gefunden
-
-          "\n"
-          compiler_builtins-5974d7451bf4ffc6.d
-          core-79b7ca805d23dd23.d
-          libcompiler_builtins-5974d7451bf4ffc6.rlib
-          libcore-79b7ca805d23dd23.rlib
-
-          "\n"
-          BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o
-
-          "\n"
-
-          "\n"
-
-          "\n"
-
-          "\n"
-
-          "\n"
-
-          "\n"
-
-          "\n"
-          BlinkySphere-e957dd655fdee2c4.25htlwio5jnie0pp.rcgu.o
-          BlinkySphere-e957dd655fdee2c4.d
-          BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o
-
-          "\n"
-
-          "\n"
-
-          "\n"
-
-          "\n"
-          compiler_builtins-5974d7451bf4ffc6.d
-          core-79b7ca805d23dd23.d
-          libcompiler_builtins-5974d7451bf4ffc6.rlib
-          libcore-79b7ca805d23dd23.rlib
-
-          "\n"
-
-          "\n"
-
-          "\n"
-          libcore-79b7ca805d23dd23.rlib
-
-          "\n"
-          libcompiler_builtins-5974d7451bf4ffc6.rlib
-
-          "\n"
-
-          "\n"
-
-          "\n"
-
-
-"-L" "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o" "-o" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4" "-L" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps" "-L" "D:\workspaces\test\target\debug\deps" "-L" "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" "--sysroot=\"C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\"" "-Wl,--no-undefined" "-nodefaultlibs" "-B \"C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\tools\gcc\"" "-march=armv7ve" "-mcpu=cortex-a7" "-mthumb" "-mfpu=neon" "-mfloat-abi=hard" "-lapplibs" "-lpthread" "-lgcc_s" "-lc"
-
-"%CCPATH%\gcc.exe" -L "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o" -o "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4" "-L" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps" "-L" "D:\workspaces\test\target\debug\deps" "-L" "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" --sysroot="C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\" "-Wl,--no-undefined" "-nodefaultlibs" -B "C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\tools\gcc\"-march=armv7ve" "-mcpu=cortex-a7" "-mthumb" "-mfpu=neon" "-mfloat-abi=hard" "-lapplibs" "-lpthread" "-lgcc_s" "-lc"
-
-"%CCPATH%\gcc.exe" -L "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o" -o "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4" "-L" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps" "-L" "D:\workspaces\test\target\debug\deps" "-L" "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" --sysroot="C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1" "-Wl,--no-undefined" "-nodefaultlibs" -B "C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\tools\gcc" -march=armv7ve "-mcpu=cortex-a7" "-mthumb" "-mfpu=neon" "-mfloat-abi=hard" "-lapplibs" "-lpthread" "-lgcc_s" "-lc"
-"%CCPATH%\gcc.exe" -L "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4.zzrb1uug1y5px4t.rcgu.o" -o "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps\BlinkySphere-e957dd655fdee2c4" "-L" "D:\workspaces\test\target\arm-v7-none-eabi\debug\deps" "-L" "D:\workspaces\test\target\debug\deps" "-L" "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" --sysroot="C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1" "-Wl,--no-undefined" "-nodefaultlibs" -B "C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\tools\gcc" -march=armv7ve "-mcpu=cortex-a7" "-mthumb" "-mfpu=neon" "-mfloat-abi=hard" "-lapplibs" "-lpthread" "-lgcc_s" "-lc"
-
-
-"%CCPATH%\gcc" -o "Blink.out" hello.o --sysroot="C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1" -Wl,--no-undefined -nodefaultlibs -B "C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\tools\gcc" -march=armv7ve -mcpu=cortex-a7 -mthumb -mfpu=neon -mfloat-abi=hard -lapplibs -lpthread -lgcc_s -lc
-
-"%CCPATH%\gcc" -L "C:\Users\bjoern\.xargo\lib\rustlib\arm-v7-none-eabi\lib" hello.o -o "Blink.out"  --sysroot="C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1" -Wl,--no-undefined -nodefaultlibs -B "C:\Program Files (x86)\Microsoft Azure Sphere SDK\Sysroots\1\tools\gcc" -march=armv7ve -mcpu=cortex-a7 -mthumb -mfpu=neon -mfloat-abi=hard -lapplibs -lpthread -lgcc_s -lc
-
-*/
--- sphere_blinky/build.rs
+++ sphere_blinky/build.rs
@@ -2,13 +2,27 @@
 
 use std::process::Command;
 use std::path::Path;
+use std::process;
+use std::str;
+
 
 fn main() {
     if !Path::new("..\\gcc_wrapper\\target\\debug\\gcc_wrapper.exe").exists() {
-        Command::new("cargo")
+        let output = Command::new("cargo")
             .args(&["build"])
             .current_dir("..\\gcc_wrapper")
-            .status()
-            .unwrap();
+            .output()
+            .expect("failed to execute process");
+
+        let out = str::from_utf8(&(output.stdout)).expect("unable to get stdout");
+        eprintln!("{}", out);
+
+        let err_out = str::from_utf8(&(output.stderr)).expect("unable to get stderr");
+        eprintln!("{}", err_out);
+
+        match output.status.code() {
+            Some(code) => process::exit(code),
+            None       => process::exit(42)
+        }
     }
  }
